c568699b07dd649eeb7b915feae603a5112f3372
jingzhef <jingzhef@hawaii.edu>
2021-09-22 13:27:58 -1000

    spamfilter

---
+++ spamFilter.js
@@ -0,0 +1,31 @@
+const test1 = {
+  truePositives: 1,
+  falsePositives: 6,
+  trueNegatives: 3,
+  falseNegatives: 2,
+}
+
+const test2 = {
+  truePositives: 6,
+  trueNegatives: 4,
+  falsePositives: 12,
+  falseNegatives: 8,
+}
+
+function analyzeSpamFilter(a1, a2) {
+  let final = [];
+
+  let finalTP = _.reduce([a1, a2], function (n, m) {return n.truePositives + m.truePositives;});
+  let finalTN = _.reduce([a1, a2], function (n, m) {return n.trueNegatives + m.trueNegatives;});
+  let finalFP = _.reduce([a1, a2], function (n, m) {return n.falsePositives + m.falsePositives;});
+  let finalFN = _.reduce([a1, a2], function (n, m) {return n.falseNegatives + m.falseNegatives;});
+  final.push(finalTP);
+  final.push(finalTN);
+  final.push(finalFP);
+  final.push(finalFN);
+
+  return final;
+
+}
+
+console.log(analyzeSpamFilter(test1, test2));
