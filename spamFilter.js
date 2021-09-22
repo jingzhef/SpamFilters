e2558ddc8c8fbb53b1beee80fd4071b377e4ecd2
jingzhef <jingzhef@hawaii.edu>
2021-09-22 13:46:32 -1000

    Update spamFilter.js

--- spamFilter.js
+++ spamFilter.js
@@ -13,16 +13,21 @@ const test2 = {
 }
 
 function analyzeSpamFilter(a1, a2) {
-  let final = [];
 
   let finalTP = _.reduce([a1, a2], function (n, m) {return n.truePositives + m.truePositives;});
   let finalTN = _.reduce([a1, a2], function (n, m) {return n.trueNegatives + m.trueNegatives;});
   let finalFP = _.reduce([a1, a2], function (n, m) {return n.falsePositives + m.falsePositives;});
   let finalFN = _.reduce([a1, a2], function (n, m) {return n.falseNegatives + m.falseNegatives;});
-  final.push(finalTP);
-  final.push(finalTN);
-  final.push(finalFP);
-  final.push(finalFN);
+  //final.push(finalTP);
+  //final.push(finalTN);
+  //final.push(finalFP);
+  //final.push(finalFN);
+  let final = {
+    TruePositives: finalTP,
+    trueNegatives: finalTN,
+    falsePositives: finalFP,
+    falseNegatives: finalFN,
+  };
 
   return final;
 
